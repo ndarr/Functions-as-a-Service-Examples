e531fee1e9747e4bb36d0a1b9728456f47e727ce
Nicolas Darr <nicolas.darr96@gmail.com>
2018-09-13 00:02:16 +0200

    Fixed import errors with OpenWhisk

--- OpenWhisk/image-resizer/__main__.py
+++ OpenWhisk/image-resizer/__main__.py
@@ -1,26 +1,35 @@
 from imageresizer import resize
-import imageio
-import matplotlib.pyplot as plt
+from PIL import Image
+import requests
+from io import BytesIO
 import base64
 import os
+import numpy as np
+import json
+
 
 
 def main(args):
+    #json.dumps(args)
+    filename = 'out.jpg'
     # Download Image
-    img = imageio.imread(args.imgurl)
+    response = requests.get(args['imgurl'])
+    img = Image.open(BytesIO(response.content))
 
+    imgRaw = np.array(img)
     #TODO: Define scale from absolute goal res like scale down to max 640x480
 
     #Resize Image
-    resizedImage = resize(img, 0.1)
+    resizedImageRaw = resize(imgRaw, 0.1)
 
     #TODO: use uuid() for unique filenames
-    plt.imsave('out.jpg', resizedImage)
+    resizedImage = Image.fromarray(resizedImageRaw)
+    resizedImage.save(filename)
 
     #Return image
-    with open('out.jpg', 'rb') as f:
+    with open(filename, 'rb') as f:
         encodeImg = base64.standard_b64encode(f.read())
 
-    os.remove('out.jpg')
+    os.remove(filename)
     #TODO: delete temporary file
     return { "headers": {"Content-Type":"image/jpg", "statusCode": 200}, "body": str(encodeImg, 'utf-8')}
--- OpenWhisk/imageResizer_deploy.sh
+++ OpenWhisk/imageResizer_deploy.sh
@@ -1,8 +1,8 @@
-sudo docker run --rm -v "$PWD:/tmp" openwhisk/python3action bash "cd tmp && virtualenv virtualenv && source virtualenv/bin/activate && pip install -r requirements.txt"
-echo "INFO: DOCKER COMPLETE"
+#sudo docker run --rm -v "$PWD:/tmp" openwhisk/python3action bash "cd tmp && virtualenv virtualenv && source virtualenv/bin/activate && pip install -r requirements.txt"
+#echo "INFO: DOCKER COMPLETE"
 cd image-resizer
-zip -r ../imageResizer.zip ./virtualenv ./__main__.py ./imageresizer.py
+zip -r ../imageResizer.zip ./virtualenv ./__main__.py ./imageresizer.py ./requirements.txt
 cd ..
 echo "INFO: ZIPPING COMPLETE"
-wsk -i action update imageResizer --kind python:3 imageResizer.zip --web true
+wsk -i action update imageResizer imageResizer.zip --docker ibmfunctions/action-python-v3 --web true
 echo "INFO: FUNCTION DEPLOYED"
